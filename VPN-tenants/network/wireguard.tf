7df1b542dcc67fb23e4b36e8616808ffee05f85e
DogRing <changh232@naver.com>
2024-07-30 17:18:12 +0900

    null_resource to terraform_data

--- VPN-tenants/network/wireguard.tf
+++ VPN-tenants/network/wireguard.tf
@@ -8,28 +8,28 @@ resource "random_string" "key" {
 locals {
   key_name = var.key_gen == "" ? random_string.key.result : var.key_gen
 }
-resource "null_resource" "generate_public" {
+resource "terraform_data" "generate_public" {
   count = var.private_key != "" ? 1 : 0
   provisioner "local-exec" {
     command = "echo ${var.private_key} | wg pubkey > ${path.module}/keys/${local.key_name}-public.key"
   }
-  triggers = { always_run = "${timestamp()}"}
+  triggers_replace = { always_run = "${timestamp()}"}
 }
 
 # key generate both
-resource "null_resource" "generate_keys" {
+resource "terraform_data" "generate_keys" {
   count = var.key_gen != "" ? 1 : 0
   provisioner "local-exec" {
     command = "wg genkey | tee ${path.module}/keys/${var.key_gen}-private.key | wg pubkey > ${path.module}/keys/${var.key_gen}-public.key"
   }
-  triggers = { always_run = "${timestamp()}"}
+  triggers_replace = { always_run = "${timestamp()}"}
 }
 data "local_file" "private_key" {
   count = var.key_gen != "" ? 1 : 0
-  depends_on = [ null_resource.generate_keys ]
+  depends_on = [ terraform_data.generate_keys ]
   filename = "${path.module}/keys/${var.key_gen}-private.key"
 }
 data "local_file" "public_key" {
-  depends_on = [ null_resource.generate_keys, null_resource.generate_public ]
+  depends_on = [ terraform_data.generate_keys, terraform_data.generate_public ]
   filename = "${path.module}/keys/${local.key_name}-public.key"
 }
