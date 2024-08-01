4ac6d122950dbd89942b59d95a37fd9df492f29e
DogRing <changh232@naver.com>
2024-08-01 14:25:10 +0900

    set multi tenant

--- Node-tenants/module.tf
+++ Node-tenants/module.tf
@@ -1,23 +1,23 @@
-# module "Node-k8s" {
-#   project_name = "k8snode"
-#   source = "./Node"
-#   providers = { azurerm = azurerm.k8s }
-#   node_count = 2
+module "Node-k8s" {
+  project_name = "k8snode"
+  source = "./Node"
+  providers = { azurerm = azurerm.k8s }
+  node_count = 1
 
-#   location = "koreacentral"
-#   address_space = "10.10.0.0/16"
-#   subnet_addresses = ["10.10.1.0/24"]
-#   route_addresses = [ "10.255.255.128/25", "192.168.0.0/24", "10.20.0.0/16" ]
+  location = "koreacentral"
+  address_space = "10.10.0.0/16"
+  subnet_addresses = ["10.10.1.0/24"]
+  route_addresses = [ "10.255.255.128/25", "192.168.0.0/24", "10.20.0.0/16" ]
 
-#   vpn_config = var.vpn_config
-#   vm_config = var.vm_config.k8s
-# }
+  vpn_config = var.vpn_config
+  vm_config = var.vm_config.k8s
+}
 
 module "Node-gpu" {
   project_name = "gpunode"
   source = "./Node"
   providers = { azurerm = azurerm.gpu }
-  node_count = 2
+  node_count = 1
 
   location = "koreacentral"
   address_space = "10.20.0.0/16"
--- Node-tenants/outputs.tf
+++ Node-tenants/outputs.tf
@@ -1,7 +1,9 @@
-output "pri-ip"{
+output "k8s-ip"{
+  value = module.Node-k8s.pri-ip
+}
+output "gpu-ip"{
   value = module.Node-gpu.pri-ip
 }
-
 locals { current_time = timeadd(timestamp(),"9h") }
 output "current_time" {
   value       = local.current_time
